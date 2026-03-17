6222bfa014a42dd1af4c9a8b49a8182e66104cd8
TechSarthak17 <2007sarthak17@gmail.com>
2026-03-17 09:47:16 +0530

    2nd commit voh wala kya pta

--- bit.java
+++ bit.java
@@ -4,10 +4,12 @@ public class bit {
 
 
     public static void main(String[] args) {
-        System.out.println("Enter your age:");
+        System.out.println("Enter a Numver 1st:\n");
         Scanner sc = new Scanner(System.in);
+
+        
         int age = sc.nextInt();
-        System.out.println("You entered: " + age);
-        sc.close();
+        System.out.println("Your age is  bababa" + age);
+
     }
 }
