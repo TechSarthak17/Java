24b394a734edc089a68a70202b3d61573047e802
TechSarthak17 <2007sarthak17@gmail.com>
2026-03-24 09:01:59 +0530

    Ajj sikha Switch case

---
+++ Count.java
@@ -0,0 +1,14 @@
+public class Count {
+    public static void main(String[] args) {
+        int i ;
+
+        for (i = 1 ; i<=100 ; i++ )
+        {
+            System.out.println(i);
+        }
+
+    
+        }
+    }
+
+ 
--- bit.java
+++ bit.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
-
+ 
 public class bit {
-
-
     public static void main(String[] args) {
-        System.out.println("Enter a Numver 1st:\n");
         Scanner sc = new Scanner(System.in);
+        float grade = sc.nextFloat();
+        if( grade > 0.00 && grade <= 100){
 
         
-        int age = sc.nextInt();
-        System.out.println("Your age is  bababa" + age);
-
+            if (grade >= 90) {
+              System.out.println("A");
+            } else if (grade >= 80) {
+              System.out.println("B");
+            } else if (grade >= 70) {
+              System.out.println("C");
+            } else if (grade >= 60) {
+              System.out.println("D");
+            } else {
+                 System.out.println("F");
+         }
+        }
+    else{
+            System.out.println("Invalid input niggaa !!!!!!!!");
+    }
+        sc.close();
     }
 }
